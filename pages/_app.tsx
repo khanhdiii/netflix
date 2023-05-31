a8182f8e3291b264916049eb9848d48206e79bea
khanhdiii <khanh12369874@gmail.com>
2023-06-01 05:51:12 +0700

    coding ui header, banner and setting

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+// _app.tsx
+
+import React from 'react'
+import '../styles/globals.css'
+
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: React.ElementType
+  pageProps: any
+}) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
