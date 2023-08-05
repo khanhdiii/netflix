ffdff1026c3cdc30a96898ca2391c2292cb258d9
khanhdiii <khanh12369874@gmail.com>
2023-08-06 06:18:45 +0700

    fix: code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useRecoilValue } from 'recoil';
 import Banner from '@/components/Banner';
 import Header from '@/components/Header';
 import Row from '@/components/Row';
@@ -5,26 +6,19 @@ import useAuth from '@/hooks/useAuth';
 import { Movie } from '@/typings';
 import requests from '@/utils/requests';
 import Head from 'next/head';
+import Modal from '@/components/Modal';
+import { modalState } from '@/atoms/modalAtom';
 import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '@/firebase';
 import Plans from '@/components/Plans';
 import { message } from 'antd';
-import Modal from '@/components/Modal';
-import { modalState } from '@/atoms/modalAtom';
-import { useRecoilValue } from 'recoil';
 
-export interface ProductProps {
+interface ProductProps {
   id: number;
   name: string;
   description: string;
-  prices?: any[];
-  metadata?: {
-    videoQuality?: string;
-    resolution?: string;
-    portability?: string;
-  };
-  selectedPlan?: any;
+  prices: any[];
 }
 
 interface Props {
@@ -41,12 +35,12 @@ interface Props {
 const Home = ({
   netflixOriginals,
   trendingNow,
-  topRated,
   actionMovies,
   comedyMovies,
+  documentaries,
   horrorMovies,
   romanceMovies,
-  documentaries,
+  topRated,
 }: Props) => {
   const { loading } = useAuth();
   const showModal = useRecoilValue(modalState);
@@ -57,21 +51,22 @@ const Home = ({
     const fetchProducts = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, 'products'));
-        const productsData: any = querySnapshot.docs?.map((doc) => {
+        const productsData: any = querySnapshot.docs.map((doc) => {
           const productData = doc.data() as ProductProps;
           productData.prices = [];
           return { ...productData, id: doc.id };
         });
 
-        const priceFetchPromises = productsData?.map(
+        const priceFetchPromises = productsData.map(
           async (productData: any) => {
             const pricesSnapshot = await getDocs(
               collection(db, `products/${productData.id}/prices`),
             );
-            const prices = pricesSnapshot.docs?.map((priceDoc) =>
+            const prices = pricesSnapshot.docs.map((priceDoc) =>
               priceDoc.data(),
             );
             productData.prices = prices;
+            message.error(`Prices for product`);
           },
         );
 
@@ -79,7 +74,7 @@ const Home = ({
 
         setProducts(productsData);
       } catch (error) {
-        message.error('Fetch data is failed');
+        message.error('Error fetching products:');
       }
     };
 
@@ -93,6 +88,7 @@ const Home = ({
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - NetFlix</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
@@ -101,7 +97,6 @@ const Home = ({
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
-
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
