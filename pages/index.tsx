8c9b720fcd12f8a554a95cd7c001bc8213bab09a
khanhdiii <khanh12369874@gmail.com>
2023-08-06 05:50:55 +0700

    fix: remove favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,7 +93,6 @@ const Home = ({
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - NetFlix</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
