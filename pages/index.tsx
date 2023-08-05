26f68de642ad9f2a1dc5aa1e7dee032d07ea85af
khanhdiii <khanh12369874@gmail.com>
2023-08-06 05:32:48 +0700

    fix: try comment code index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header';
 import Row from '@/components/Row';
 import useAuth from '@/hooks/useAuth';
 import { Movie } from '@/typings';
-import requests from '@/utils/requests';
+// import requests from '@/utils/requests';
 import Head from 'next/head';
 import Modal from '@/components/Modal';
 import { modalState } from '@/atoms/modalAtom';
@@ -116,37 +116,37 @@ const Home = ({
 
 export default Home;
 
-export const getServerSideProps = async () => {
-  const [
-    netflixOriginals,
-    trendingNow,
-    topRated,
-    actionMovies,
-    comedyMovies,
-    horrorMovies,
-    romanceMovies,
-    documentaries,
-  ] = await Promise.all([
-    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-    fetch(requests.fetchTrending).then((res) => res.json()),
-    fetch(requests.fetchTopRated).then((res) => res.json()),
-    fetch(requests.fetchActionMovies).then((res) => res.json()),
-    fetch(requests.fetchComedyMovies).then((res) => res.json()),
-    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ]);
+// export const getServerSideProps = async () => {
+//   const [
+//     netflixOriginals,
+//     trendingNow,
+//     topRated,
+//     actionMovies,
+//     comedyMovies,
+//     horrorMovies,
+//     romanceMovies,
+//     documentaries,
+//   ] = await Promise.all([
+//     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+//     fetch(requests.fetchTrending).then((res) => res.json()),
+//     fetch(requests.fetchTopRated).then((res) => res.json()),
+//     fetch(requests.fetchActionMovies).then((res) => res.json()),
+//     fetch(requests.fetchComedyMovies).then((res) => res.json()),
+//     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+//     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+//     fetch(requests.fetchDocumentaries).then((res) => res.json()),
+//   ]);
 
-  return {
-    props: {
-      netflixOriginals: netflixOriginals.results,
-      trendingNow: trendingNow.results,
-      topRated: topRated.results,
-      actionMovies: actionMovies.results,
-      comedyMovies: comedyMovies.results,
-      horrorMovies: horrorMovies.results,
-      romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,
-    },
-  };
-};
+//   return {
+//     props: {
+//       netflixOriginals: netflixOriginals.results,
+//       trendingNow: trendingNow.results,
+//       topRated: topRated.results,
+//       actionMovies: actionMovies.results,
+//       comedyMovies: comedyMovies.results,
+//       horrorMovies: horrorMovies.results,
+//       romanceMovies: romanceMovies.results,
+//       documentaries: documentaries.results,
+//     },
+//   };
+// };
